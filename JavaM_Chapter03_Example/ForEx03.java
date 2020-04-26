49214930a83bc2d294294995525f6941bac5fafc
ITneko <uneko15@gmail.com>
2020-04-26 21:56:17 +0900

    2020-04-26

---
+++ JavaM_Chapter03_Example/ForEx03.java
@@ -0,0 +1,25 @@
+package JavaM_Chapter03_Example;
+
+public class ForEx03 {
+
+	public static void main(String[] args) {
+
+		int i, j, k; // k=단수, j=단의 변화수, i=3단씩 증가
+
+		for (i = 1; i < 9; i += 3) {
+			for (j = 1; j <= 9; ++j) {
+				if(j==1) {
+					for(k=i; k<=i+2; k++)
+						System.out.printf("%d단\t\t",k);
+					System.out.println();
+				}
+ 				for (k = i; k <= i + 2; k++)
+					System.out.printf("%d * %d = %2d \t", k, j, k * j);
+				System.out.printf("\n");
+			}
+			System.out.printf("\n");
+		}
+		System.out.printf("\n");
+	}
+
+}
