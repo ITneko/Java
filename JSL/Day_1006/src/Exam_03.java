664dcca1c1408920a286827936a8e2cb9b481d53
ITneko <uneko15@gmail.com>
2020-10-06 22:14:19 +0900

    1006

--- JSL/Day_1006/src/Exam_03.java
+++ JSL/Day_1006/src/Exam_03.java
@@ -4,29 +4,27 @@ import java.util.StringTokenizer;
 
 public class Exam_03 {
 
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args) throws Exception{
 		StringTokenizer st = null;
 		try {
 			Scanner sc = new Scanner(new File("/Users/uneko/data/data.txt"));
-			while (sc.hasNextLine()) {
+			while(sc.hasNextLine()) {
 				String ss = sc.nextLine();
-				st = new StringTokenizer(ss, ", -");
-				int val[] = new int[3];
-				while (st.hasMoreTokens()) {
+				st = new StringTokenizer(ss, " ,-");
+				int sum=0;
+				while(st.hasMoreTokens()) {
 					String temp = st.nextToken();
 					System.out.print(temp + "\t");
-					if(st.countTokens() < 3 ) {
-						val[st.countTokens()] = Integer.parseInt(temp);
-					}
+					
+					if(st.countTokens() < 3) 
+						sum+= Integer.parseInt(temp);
 				}
-				int sum = val[0]+val[1]+val[2];
-				System.out.print(sum + "\t" + Math.round(sum*10./3)/10.);
-				System.out.println();
-				
-				
+				System.out.print(sum +"\t");
+				System.out.println(Math.round(sum*10./3)/10.);
 			}
-		} catch (Exception e) {
-			System.out.println("파일없음");
+			
+		}catch(Exception e) {
+			System.out.println("파일이 없습니다.");
 		}
 	}
 
